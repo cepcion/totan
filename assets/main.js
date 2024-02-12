a0f62feffca1321b5853e17dbe2638f1cbda31ec
Paul C <cepcion@gmail.com>
2024-02-12 02:59:37 -0600

    totanpath

--- assets/main.js
+++ assets/main.js
@@ -6485,7 +6485,8 @@ const totan = {
 };
 
 const Uc = [
-    { path: "/", name: "talesOfArabianNights", component: totan }
+    { path: "/", name: "talesOfArabianNights", component: totan },
+    { path: "/totan", name: "totan", component: totan }    
   ],
   Nc = G({ history: U(), routes: Uc });
 N(mainApp).use(Nc).mount("#app");
